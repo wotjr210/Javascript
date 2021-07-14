b28ac0ac4c504558e0854f9115c24460eafa79a3
wotjr210 <wotjr210@.naver.com>
2021-07-15 01:05:56 +0900

    JavaScript Include

---
+++ main.js
@@ -0,0 +1,6 @@
+// typescript에서는 선언한 필요 없음.
+// Javascript is very flexible.
+// flexible === dengerous
+// added ECMAScript5
+// Javascript egnine이 더빠르고 효율적으로 분석할 수 있기때문에, 조금더 나은 성능개선.
+'use strict'
